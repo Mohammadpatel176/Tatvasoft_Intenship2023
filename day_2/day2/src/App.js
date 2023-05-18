3c1e863c52ad72e360d018dbc13e79bdf3c0ffb8
Mohammadpatel176 <mohammadpatel7402@gmail.com>
2023-05-18 13:37:07 +0530

    'second_commit'

--- day_2/day2/src/App.js
+++ day_2/day2/src/App.js
@@ -1,18 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from './HomePage';
 import { Apple } from './Apple';
+import {NAN} from './PageNotFound';
 
 const App = () => (
 
   <BrowserRouter>
-    <div>
-      <Link to='/'>Home</Link>
-      <Link to='/apple'>Apple</Link>
-    </div>
     <Routes>
       <Route path='/' element={<HomePage />}></Route>
       <Route path='/apple' element={<Apple />}></Route>
+      <Route path='*' element={<NAN />}></Route>
     </Routes>
   </BrowserRouter>
 
