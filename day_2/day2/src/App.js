834add04595cfd73b789532e9a88e89ae514ca17
Mohammadpatel176 <mohammadpatel7402@gmail.com>
2023-05-18 12:04:23 +0530

    second commit

---
+++ day_2/day2/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { HomePage } from './HomePage';
+import { Apple } from './Apple';
+
+function App() {
+  <div>
+    <Link to='/' style={{
+      marginLeft: 5,
+    }}>Home</Link>
+    <Link to='/apple' style={{
+      marginLeft: 5,
+    }}>Home</Link>
+    <Link to='/' style={{
+      marginLeft: 5,
+    }}>Home</Link>
+    
+  </div>
+}
+
+export default App;
---
+++ day_2/day2/src/Apple.js
@@ -0,0 +1,5 @@
+export const Apple = () => {
+    return (<div>
+        <p>Welcome to Apple Page</p>
+    </div>);
+}
