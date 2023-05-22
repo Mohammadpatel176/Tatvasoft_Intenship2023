48ab95419a1b430a00e5ff5a37d6b1d55d9fe4be
Mohammadpatel176 <mohammadpatel7402@gmail.com>
2023-05-22 13:10:04 +0530

    fourth commit

--- day_3/day3/src/App.js
+++ day_3/day3/src/App.js
@@ -4,23 +4,31 @@ import { HomePage } from './HomePage';
 import { Apple } from './Apple';
 import { NAN } from './PageNotFound';
 import { globalStyle } from './contains';
-
+import m1 from './image/m1.jpg';
 const App = () => (
-
-  <BrowserRouter>
-    <div style={{
-      ...globalStyle.navbar
-    }}>
-      <Link to='/' /*</div>style={{ marginLeft: 5, color: 'blue', padding: 20, fontSize: 20 }} */>Home</Link>
-      <Link to='apple'>Apple</Link>
-      <Link to='/applet'>Page Not found</Link>
+  <div>
+    <div>
+      <img src={m1} alt='my-photo'></img>
+      <img src='http://localhost:3000/logo192.png' alt='app logo'></img>
+      <img src={'${process.env.REACT_APP_HOSTED_URL}logo192.png'} alt='logo' ></img>
     </div>
-    <Routes className="Route-style">
-      <Route path='/' element={<HomePage />}></Route>
-      <Route path='/apple' element={<Apple />}></Route>
-      <Route path='*' element={<NAN />}></Route>
-    </Routes>
-  </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <div style={{
+          ...globalStyle.navbar
+        }}>
+          <Link to='/' /*</div>style={{ marginLeft: 5, color: 'blue', padding: 20, fontSize: 20 }} */>Home</Link>
+          <Link to='apple'>Apple</Link>
+          <Link to='/applet'>Page Not found</Link>
+        </div>
+        <Routes className="Route-style">
+          <Route path='/' element={<HomePage />}></Route>
+          <Route path='/apple' element={<Apple />}></Route>
+          <Route path='*' element={<NAN />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  </div>
 
 );
 export default App;
--- day_3/day3/src/Apple.js
+++ day_3/day3/src/Apple.js
@@ -1,12 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import AppStyle from './AppStyle.module.css';
 
 export const Apple = () => {
-    const Navigat =useNavigate();
-    const onHomePageButtonClick=()=>{
+    const Navigat = useNavigate();
+    const onHomePageButtonClick = () => {
         Navigat('/');
+    }
+    const alt = () => {
+        alert("Please choose another button");
     }
     return (<div>
         <p>Welcome to Apple Page</p>
         <button onClick={onHomePageButtonClick}>Navigate to Home page</button>
+        <button className={AppStyle.error} onClick={alt}>error button</button>
     </div>);
 }
